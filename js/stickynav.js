1821a3258e42856c3415d29b9a03d24809db890e
cmerchant1598 <cmerchant1598@gmail.com>
2018-06-09 23:55:07 -0400

    Add files via upload

---
+++ js/stickynav.js
@@ -0,0 +1,17 @@
+// Execute function when user scrolls.
+window.onscroll = function() {myFunction()};
+
+// Grab navigation bar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
